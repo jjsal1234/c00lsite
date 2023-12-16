0a4e6a8bc8f08c734bc3291391c36a7aeed9ac01
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-15 23:54:14 -0700

    Create app.js

---
+++ Js/app.js
@@ -0,0 +1,26 @@
+const paths = {
+    home: '/',
+    about: '/about',
+    games: '/games',
+    contact: '/contact',
+};
+
+// Add event listeners to navigation links
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const path = this.getAttribute('href');
+            navigateTo(path);
+        });
+    });
+});
+
+// Function to navigate to the specified path
+function navigateTo(path) {
+    // Update the location hash to trigger navigation
+    window.location.hash = path;
+    // You can add more logic here, such as updating content based on the path
+}
